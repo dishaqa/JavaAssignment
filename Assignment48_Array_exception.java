484988ecd11589e6d9d92e5fe7bd156a82ad3aa7
dishaqa <disha.s.chauhan@gmail.com>
2024-08-11 22:17:07 +0530

    Update Assignment48_Array_exception.java

--- Assignment48_Array_exception.java
+++ Assignment48_Array_exception.java
@@ -11,17 +11,29 @@ public class Assignment48_Array_exception
 		System.out.println("Enter the size of Array");
 		Scanner s1 = new Scanner(System.in);
 		
-		int i = s1.nextInt();
+		int i;
+		try 
+		{
+		i = s1.nextInt();
+		}
 		
-		
-		
+		catch (InputMismatchException a1)//Handled InputMismatchException
+		{
+			System.out.println("Hey please check the input and add it properly");
+			Scanner s2 =new Scanner(System.in);
+			i = s2.nextInt();
+			//s2.close();
+		}
+		//System.out.println(i);
 		double rollno[]= new double[i];
+		//System.out.println(rollno.length);
+		Scanner s3 =new Scanner(System.in);
 		for (int j=0;j<=rollno.length; j++)
 		{
 			System.out.println("enter value of rollno "+(j+1));
 			try
 			{
-				rollno[j] = s1.nextDouble();
+				rollno[j] = s3.nextDouble();
 			}
 			catch (ArrayIndexOutOfBoundsException a1) //Handled ArrayIndexOutOfBoundsException
 			{
@@ -31,6 +43,7 @@ public class Assignment48_Array_exception
 		}
 		System.out.println(Arrays.toString(rollno));
 		s1.close();
+	
 	}
 	
 }
