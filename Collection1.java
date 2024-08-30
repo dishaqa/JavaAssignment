637732c9a5b890e7c7a19f63a80e185e9e4a16db
dishaqa <disha.s.chauhan@gmail.com>
2024-08-30 21:35:37 +0530

    Add files via upload

--- Collection1.java
+++ Collection1.java
@@ -1,5 +1,6 @@
 package Collection;
 import	java.util.Collection;
+import java.util.Collections;
 import java.util.ArrayList;
 
 public class Collection1 
@@ -8,12 +9,21 @@ public class Collection1
 	public static void main(String[] args)
 	{
 	Collection c1 = new ArrayList();
+
 	c1.add("Age");
 	c1.add("Name");
+	c1.add("Name");
+	c1.add("Name");
+	c1.add(null);
+	c1.add(null);
+	c1.add(null);
 	c1.add(39);
 	c1.add(false);
 	c1.add(8.8);
+	
 	System.out.println(c1);
+	
+	
 	
 	Collection c2 = new ArrayList();
 	c2.add("Rice");
